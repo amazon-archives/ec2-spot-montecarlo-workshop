af98aa4ab1c06b5d3eed0f3ed42546083b5e4203
Shawn OConnor <shawo@amazon.com>
2017-11-17 00:53:53 -0800

    removed Log redirection from worker bootstrap script

--- templates/spotlabworker.sh
+++ templates/spotlabworker.sh
@@ -1,6 +1,4 @@
-#!/bin/bash -ex
-# output user data logs into a separate place for debugging
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+#!/bin/bash
 yum -y install git python-numpy python-matplotlib python-zmq python-scipy
 pip install pandas-datareader scipy
 
@@ -9,9 +7,9 @@ REGION=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|gr
 mkdir /home/ec2-user/spotlabworker
 chown ec2-user:ec2-user /home/ec2-user/spotlabworker
 cd /home/ec2-user/spotlabworker
-aws configure set default.region $REGION
+
 wget https://s3-us-west-2.amazonaws.com/reinvent2017-cmp316/queue_processor.py
 wget https://s3-us-west-2.amazonaws.com/reinvent2017-cmp316/worker.py
 echo "QUEUE = '<REPLACE WITH YOUR SQS QUEUE NAME>'" > config.py
-
+aws configure set default.region $REGION
 python /home/ec2-user/spotlabworker/queue_processor.py > stdout.txt 2>&1
