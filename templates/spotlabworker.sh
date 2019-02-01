7128fc46d01c6f66c78e0f1d564436efeac0ccad
Carlos Manzanedo Rueda <46894578+ruecarlo@users.noreply.github.com>
2019-02-01 02:54:41 +0000

    Development (#1)
    * Changed Dockerfile to avoid creating unnecessary layers for python installation
    
    * Removed github references; Using relative references within the same repo
    
    * fixed issue with template link and bold items in template table
    
    * workers were failing due to missing pip as a dependency. Added pip and flatten the dependencies into a single execution
    
    * Applied same changes to the spotlabworker.sh
    
    * added awscli to dependencies (running into this issue with botocore update https://github.com/aws/aws-cli/issues/3542)
    
    * Adding fix_yahoo_finance to the list of dependencies
    
    * docker image pointing to the fixed repo in dockerhub

--- templates/spotlabworker.sh
+++ templates/spotlabworker.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
-	# Install Dependencies
-	yum -y install git python-numpy python-matplotlib python-scipy
-	pip install pandas-datareader fix_yahoo_finance
-	pip install scipy 
-	pip install boto3
-	
-	#Populate Variables
-    echo 'Populating Variables'
-	REGION=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
-	mkdir /home/ec2-user/spotlabworker
-	chown ec2-user:ec2-user /home/ec2-user/spotlabworker
-	cd /home/ec2-user/spotlabworker
-	WEBURL=$(aws cloudformation --region $REGION describe-stacks --query 'Stacks[0].Outputs[?OutputKey==`WebInterface`].OutputValue' --output text)
-	echo 'Region is '$REGION
-    echo 'URL is '$WEBURL
+# Install Dependencies
+yum -y install git python-numpy python-matplotlib python-scipy python-pip
+pip install --upgrade pandas-datareader fix_yahoo_finance scipy boto3 awscli
 
-    echo "Downloading worker code"
-	wget $WEBURL/static/queue_processor.py
-	wget $WEBURL/static/worker.py
-	
-	echo 'Starting the worker processor'
-	python /home/ec2-user/spotlabworker/queue_processor.py --region $REGION> stdout.txt 2>&1
+#Populate Variables
+echo 'Populating Variables'
+REGION=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+mkdir /home/ec2-user/spotlabworker
+chown ec2-user:ec2-user /home/ec2-user/spotlabworker
+cd /home/ec2-user/spotlabworker
+WEBURL=$(aws cloudformation --region $REGION describe-stacks --query 'Stacks[0].Outputs[?OutputKey==`WebInterface`].OutputValue' --output text)
+echo 'Region is '$REGION
+echo 'URL is '$WEBURL
+
+echo "Downloading worker code"
+wget $WEBURL/static/queue_processor.py
+wget $WEBURL/static/worker.py
+
+echo 'Starting the worker processor'
+python /home/ec2-user/spotlabworker/queue_processor.py --region $REGION> stdout.txt 2>&1
